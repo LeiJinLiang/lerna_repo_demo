5b103faefcf7e10fb28c419a7bfc5dad75dc0a9c
Andy.L.Jin <Andy.L.Jin@newegg.com>
2019-08-15 13:15:56 +0800

    update

--- packages/ngm-www-widget/src/index.ts
+++ packages/ngm-www-widget/src/index.ts
@@ -3,3 +3,13 @@ export function cli() {
     console.log('new 00000000.2')
     awesomeFn();
 }
+
+class Foo{
+    private name: string;
+    constructor(name: string){
+        this.name = name;
+    }
+    doSth() : void{
+        console.log('run dosth')
+    }
+}
